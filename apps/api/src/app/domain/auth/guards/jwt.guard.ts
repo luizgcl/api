98c6f9303a6510b78862ce8cac77388c1585b025
luizgcl <luiizgcl@gmail.com>
2024-11-15 02:46:46 -0300

    fix lint

--- apps/api/src/app/domain/auth/guards/jwt.guard.ts
+++ apps/api/src/app/domain/auth/guards/jwt.guard.ts
@@ -12,9 +12,7 @@ export class JwtGuard extends AuthGuard('jwt') {
     super()
   }
 
-  canActivate(
-    context: ExecutionContext
-  ) {
+  canActivate(context: ExecutionContext) {
     const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
       context.getHandler(),
       context.getClass(),
